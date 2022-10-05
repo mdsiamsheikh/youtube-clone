1096dbe34844fcb7e071de83f672934a07983c5f
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2022-10-05 11:59:07 +0600

    navbar

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header-vue></header-vue>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import headerVue from "../components/header.vue";
+export default {
+  components: {
+    headerVue,
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="contaner">
+    <left-sidebad></left-sidebad>
+    <middle-sidebar></middle-sidebar>
+  </div>
+</template>
+
+<script>
+import LeftSidebad from "../components/leftSidebad.vue";
+import MiddleSidebar from "../components/middleSidebar.vue";
+export default {
+  name: "IndexPage",
+  components: {
+    LeftSidebad,
+    MiddleSidebar,
+  },
+};
+</script>
+
+<style></style>
