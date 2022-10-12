ba281b4ad91eec43df2f91f288409742768554aa
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2022-10-13 02:13:06 +0600

    sidebar ein

--- components/leftSidebad.vue
+++ components/leftSidebad.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sidebar-left">
     <div class="sidebar-categories">
-      <div class="sidbar-categoru">
+      <div class="sidbar_categorus">
         <svg
           viewBox="0 0 24 24"
           preserveAspectRatio="xMidYMid meet"
@@ -75,6 +75,7 @@
         <h4>Subscription</h4>
       </div>
     </div>
+
     <hr />
 
     <div class="sidebar-categoriess">
@@ -163,8 +164,146 @@
         </svg>
         <h4>Liked videos</h4>
       </div>
-      <hr />
     </div>
+    <hr />
+
+    <div class="explore">
+      <div class="md_1">
+        <span>Explore</span>
+      </div>
+      <div class="items">
+        <div class="items_1">
+          <a href="#" class="items_2"
+            ><svg
+              viewBox="0 0 24 24"
+              preserveAspectRatio="xMidYMid meet"
+              focusable="false"
+              class="style-scope yt-icon"
+              style="
+                pointer-events: none;
+                display: block;
+                width: 24;
+                height: 24;
+              "
+            >
+              <g class="style-scope yt-icon">
+                <path
+                  d="M10,12H8v2H6v-2H4v-2h2V8h2v2h2V12z M17,12.5c0-0.83-0.67-1.5-1.5-1.5S14,11.67,14,12.5c0,0.83,0.67,1.5,1.5,1.5 S17,13.33,17,12.5z M20,9.5C20,8.67,19.33,8,18.5,8S17,8.67,17,9.5c0,0.83,0.67,1.5,1.5,1.5S20,10.33,20,9.5z M16.97,5.15l-4.5,2.53 l-0.49,0.27l-0.49-0.27l-4.5-2.53L3,7.39v6.43l8.98,5.04l8.98-5.04V7.39L16.97,5.15 M16.97,4l4.99,2.8v7.6L11.98,20L2,14.4V6.8 L6.99,4l4.99,2.8L16.97,4L16.97,4z"
+                  class="style-scope yt-icon"
+                ></path>
+              </g>
+            </svg>
+            <span> Gaming </span>
+          </a>
+        </div>
+        <div class="items_1">
+          <a href="#" class="items_2"
+            ><svg
+              viewBox="0 0 24 24"
+              preserveAspectRatio="xMidYMid meet"
+              focusable="false"
+              class="style-scope yt-icon"
+              style="
+                pointer-events: none;
+                display: block;
+                width: 24;
+                height: 24;
+              "
+            >
+              <g class="style-scope yt-icon">
+                <path
+                  d="M17 3V5V6V10V10.51L16.99 10.97C16.94 13.1 15.66 14.94 13.74 15.67H13.72L13.66 15.69L13 15.95V16.65V19V20H14V21H15H10V20H11V19V16.65V15.95L10.34 15.71L10.26 15.68H10.25C8.33 14.95 7.05 13.11 7 10.98V10.51V10V6V5V3H17ZM18 2H6V5H4V6V10V11H6.01C6.07 13.53 7.63 15.78 9.97 16.64C9.98 16.64 9.99 16.64 10 16.65V19H9V20H8V22H16V20H15V19H14V16.65C14.01 16.65 14.02 16.65 14.03 16.64C16.36 15.78 17.93 13.54 17.99 11H20V10V6V5H18V2ZM18 10V6H19V10H18ZM5 10V6H6V10H5Z"
+                  class="style-scope yt-icon"
+                ></path>
+              </g>
+            </svg>
+            <span> Sports</span>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <hr />
+
+    <div class="settings_item">
+      <div class="active">
+        <a href="#" class="items">
+          <svg
+            viewBox="0 0 24 24"
+            preserveAspectRatio="xMidYMid meet"
+            focusable="false"
+            class="style-scope yt-icon"
+            style="pointer-events: none; display: block; width: 24; height: 24"
+          >
+            <g class="style-scope yt-icon">
+              <path
+                d="M12,9c1.65,0,3,1.35,3,3s-1.35,3-3,3s-3-1.35-3-3S10.35,9,12,9 M12,8c-2.21,0-4,1.79-4,4s1.79,4,4,4s4-1.79,4-4 S14.21,8,12,8L12,8z M13.22,3l0.55,2.2l0.13,0.51l0.5,0.18c0.61,0.23,1.19,0.56,1.72,0.98l0.4,0.32l0.5-0.14l2.17-0.62l1.22,2.11 l-1.63,1.59l-0.37,0.36l0.08,0.51c0.05,0.32,0.08,0.64,0.08,0.98s-0.03,0.66-0.08,0.98l-0.08,0.51l0.37,0.36l1.63,1.59l-1.22,2.11 l-2.17-0.62l-0.5-0.14l-0.4,0.32c-0.53,0.43-1.11,0.76-1.72,0.98l-0.5,0.18l-0.13,0.51L13.22,21h-2.44l-0.55-2.2l-0.13-0.51 l-0.5-0.18C9,17.88,8.42,17.55,7.88,17.12l-0.4-0.32l-0.5,0.14l-2.17,0.62L3.6,15.44l1.63-1.59l0.37-0.36l-0.08-0.51 C5.47,12.66,5.44,12.33,5.44,12s0.03-0.66,0.08-0.98l0.08-0.51l-0.37-0.36L3.6,8.56l1.22-2.11l2.17,0.62l0.5,0.14l0.4-0.32 C8.42,6.45,9,6.12,9.61,5.9l0.5-0.18l0.13-0.51L10.78,3H13.22 M14,2h-4L9.26,4.96c-0.73,0.27-1.4,0.66-2,1.14L4.34,5.27l-2,3.46 l2.19,2.13C4.47,11.23,4.44,11.61,4.44,12s0.03,0.77,0.09,1.14l-2.19,2.13l2,3.46l2.92-0.83c0.6,0.48,1.27,0.87,2,1.14L10,22h4 l0.74-2.96c0.73-0.27,1.4-0.66,2-1.14l2.92,0.83l2-3.46l-2.19-2.13c0.06-0.37,0.09-0.75,0.09-1.14s-0.03-0.77-0.09-1.14l2.19-2.13 l-2-3.46L16.74,6.1c-0.6-0.48-1.27-0.87-2-1.14L14,2L14,2z"
+                class="style-scope yt-icon"
+              ></path>
+            </g>
+          </svg>
+          <span>Settings</span>
+        </a>
+      </div>
+      <div class="active">
+        <a href="#" class="items">
+          <svg
+            viewBox="0 0 24 24"
+            preserveAspectRatio="xMidYMid meet"
+            focusable="false"
+            class="style-scope yt-icon"
+            style="pointer-events: none; display: block; width: 24; height: 24"
+          >
+            <g class="style-scope yt-icon">
+              <path
+                d="M13.18,4l0.24,1.2L13.58,6h0.82H19v7h-5.18l-0.24-1.2L13.42,11H12.6H6V4H13.18 M14,3H5v18h1v-9h6.6l0.4,2h7V5h-5.6L14,3 L14,3z"
+                class="style-scope yt-icon"
+              ></path>
+            </g>
+          </svg>
+          <span>Report history</span>
+        </a>
+      </div>
+      <div class="active">
+        <a href="#" class="items">
+          <svg
+            viewBox="0 0 24 24"
+            preserveAspectRatio="xMidYMid meet"
+            focusable="false"
+            class="style-scope yt-icon"
+            style="pointer-events: none; display: block; width: 24; height: 24"
+          >
+            <g class="style-scope yt-icon">
+              <path
+                d="M15.36,9.96c0,1.09-0.67,1.67-1.31,2.24c-0.53,0.47-1.03,0.9-1.16,1.6L12.85,14h-1.75l0.03-0.28 c0.14-1.17,0.8-1.76,1.47-2.27c0.52-0.4,1.01-0.77,1.01-1.49c0-0.51-0.23-0.97-0.63-1.29c-0.4-0.31-0.92-0.42-1.42-0.29 c-0.59,0.15-1.05,0.67-1.19,1.34L10.32,10H8.57l0.06-0.42c0.2-1.4,1.15-2.53,2.42-2.87c1.05-0.29,2.14-0.08,2.98,0.57 C14.88,7.92,15.36,8.9,15.36,9.96z M12,18c0.55,0,1-0.45,1-1s-0.45-1-1-1s-1,0.45-1,1S11.45,18,12,18z M12,3c-4.96,0-9,4.04-9,9 s4.04,9,9,9s9-4.04,9-9S16.96,3,12,3 M12,2c5.52,0,10,4.48,10,10s-4.48,10-10,10S2,17.52,2,12S6.48,2,12,2L12,2z"
+                class="style-scope yt-icon"
+              ></path>
+            </g>
+          </svg>
+          <span>Help</span>
+        </a>
+      </div>
+      <div class="active">
+        <a href="#" class="items">
+          <svg
+            viewBox="0 0 24 24"
+            preserveAspectRatio="xMidYMid meet"
+            focusable="false"
+            class="style-scope yt-icon"
+            style="pointer-events: none; display: block; width: 24; height: 24"
+          >
+            <g class="style-scope yt-icon">
+              <path
+                d="M13,14h-2v-2h2V14z M13,5h-2v6h2V5z M19,3H5v16.59l3.29-3.29L8.59,16H9h10V3 M20,2v15H9l-5,5V2H20L20,2z"
+                class="style-scope yt-icon"
+              ></path>
+            </g>
+          </svg>
+          <span>Send feedback</span>
+        </a>
+      </div>
+    </div>
+    <hr />
   </div>
 </template>
 
@@ -178,6 +317,24 @@ export default {};
   width: 230px;
   background: #ffffff;
   overflow-y: scroll;
+  font-family: "Roboto", sans-serif;
+}
+
+.sidbar_categorus {
+  display: flex;
+  align-items: center;
+  padding: 10px 25px;
+  background: #e5e5e5;
+}
+
+.sidbar_categorus:hover {
+  background: #dcd9d9;
+}
+
+.sidbar_categorus h4 {
+  margin-left: 22px;
+  font-size: 15px;
+  color: rgb(39, 35, 35);
 }
 
 .sidebar-categories {
@@ -193,18 +350,114 @@ export default {};
   padding: 10px 25px;
 }
 
-.sidbar-categoru svg {
-  width: 100px;
-  height: 100px;
-}
-
 .sidbar-categoru h4 {
   margin-left: 22px;
-  font-size: 16px;
-  color: rgb(63, 59, 59);
+  font-size: 14px;
+  color: rgb(54, 48, 48);
 }
 
 .sidbar-categoru:hover {
   background: #e0dddd;
+  cursor: pointer;
+}
+
+.sidebar-categoriess {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+  margin-top: 7px;
+}
+
+.sidbar-categorus {
+  display: flex;
+  align-items: center;
+  padding: 10px 25px;
+}
+
+.sidbar-categorus h4 {
+  margin-left: 22px;
+  font-size: 14px;
+  color: rgb(71, 65, 65);
+}
+
+.sidebar-left::-webkit-scrollbar {
+  display: none;
+}
+
+.sidbar-categorus:hover {
+  background: #e0dddd;
+}
+
+.items_1 a {
+  text-decoration: none;
+  color: #333;
+}
+
+.md_1 {
+  display: flex;
+  padding: 2px 22px;
+  margin-top: 10px;
+}
+
+.md_1 span {
+  font-size: 16px;
+  margin-bottom: 8px;
+  font-family: "Roboto", sans-serif;
+}
+
+.items_1 span {
+  margin-left: 20px;
+  font-family: "Roboto", sans-serif;
+}
+
+.items_1 svg {
+  width: 24px;
+  height: 24px;
+}
+
+.items_1 .items_2 {
+  display: flex;
+  align-items: center;
+  margin-bottom: 2px;
+  margin-left: 2px;
+  padding: 10px 25px;
+}
+
+.items_1:hover {
+  background: #e0dddd;
+}
+
+.settings_item {
+  display: flex;
+  flex-direction: column;
+  margin-top: 16px;
+}
+
+.active .items {
+  display: flex;
+  align-items: center;
+  margin-bottom: 2px;
+  margin-left: 2px;
+  padding: 10px 25px;
+}
+
+.active span {
+  margin-left: 20px;
+  font-family: "Roboto", sans-serif;
+}
+
+.active a {
+  text-decoration: none;
+  color: #333;
+}
+
+.active:hover {
+  background: #e0dddd;
+}
+
+hr {
+  height: 1px;
+  background-color: #dcd9d9;
+  border: none;
 }
 </style>
