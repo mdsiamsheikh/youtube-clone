4bf1072d8b89986d008db82c360f1a0915583bff
siam <mdsiamsheikh6640@gmail.com>
2024-03-07 00:07:46 +0600

    creat a new navbar search bar

--- components/header.vue
+++ components/header.vue
@@ -75,9 +75,51 @@
         </g>
       </svg>
     </div>
+    <!-- <div class="header_search">
+      <div class="header-searc-button">
+        <div class="action">
+          <input type="text" placeholder="Seach" />
+        </div>
+        <button>
+          <svg
+            viewBox="0 0 24 24"
+            preserveAspectRatio="xMidYMid meet"
+            focusable="false"
+            class="style-scope yt-icon"
+            style="pointer-events: none; display: block; width: 25; height: 30"
+          >
+            <g class="style-scope yt-icon">
+              <path
+                d="M20.87,20.17l-5.59-5.59C16.35,13.35,17,11.75,17,10c0-3.87-3.13-7-7-7s-7,3.13-7,7s3.13,7,7,7c1.75,0,3.35-0.65,4.58-1.71 l5.59,5.59L20.87,20.17z M10,16c-3.31,0-6-2.69-6-6s2.69-6,6-6s6,2.69,6,6S13.31,16,10,16z"
+                class="style-scope yt-icon"
+              ></path>
+            </g>
+          </svg>
+        </button>
+      </div>
+      <div class="miet">
+        <svg
+          viewBox="0 0 24 24"
+          preserveAspectRatio="xMidYMid meet"
+          focusable="false"
+          class="style-scope yt-icon"
+          style="pointer-events: none; display: block; width: 27; height: 27"
+        >
+          <g class="style-scope yt-icon">
+            <path
+              d="M12 3C10.34 3 9 4.37 9 6.07V11.93C9 13.63 10.34 15 12 15C13.66 15 15 13.63 15 11.93V6.07C15 4.37 13.66 3 12 3ZM18.5 12H17.5C17.5 15.03 15.03 17.5 12 17.5C8.97 17.5 6.5 15.03 6.5 12H5.5C5.5 15.24 7.89 17.93 11 18.41V21H13V18.41C16.11 17.93 18.5 15.24 18.5 12Z"
+              class="style-scope yt-icon"
+            ></path>
+          </g>
+        </svg>
+      </div>
+    </div> -->
+
     <div class="header_search">
-      <div class="action">
-        <input type="text" placeholder="Seach" />
+      <div class="header-search-button">
+        <div class="action">
+          <input type="text" placeholder="Search" class="search-input" />
+        </div>
         <button>
           <svg
             viewBox="0 0 24 24"
@@ -197,18 +239,28 @@ body {
   cursor: pointer;
 }
 
-.header_search {
+/* .header_search {
   display: flex;
   align-items: center;
 }
 
 .header_search .action {
-  border: 1px solid #c6c6c6;
+  border: 1px solid #cfcbcb;
   height: 35px;
   margin: 0;
   padding: 0;
   display: flex;
   position: relative;
+}
+
+.header-searc-button {
+  display: flex;
+  height: 35px;
+}
+
+.action {
+  border: #f9f9f9;
+  border-radius: 5rem;
 }
 
 .header_search input {
@@ -243,6 +295,63 @@ body {
   margin-left: 15px;
   cursor: pointer;
   background: #f9f9f9;
+  border-radius: 50%;
+  padding: 7px;
+} */
+
+.header_search {
+  display: flex;
+  align-items: center;
+}
+
+.header_search input {
+  width: 450px;
+  padding: 10px;
+  font-size: 16px;
+}
+
+.header-search-button {
+  display: flex;
+  align-items: center;
+  border-radius: 4rem;
+  height: 40px;
+  border: 1px solid #afabab;
+  overflow: hidden;
+}
+
+.action {
+  padding: 8px;
+}
+
+.search-input {
+  border: none;
+  outline: none;
+  width: 100%;
+  padding: 8px;
+}
+
+button {
+  background-color: #f1f1f1;
+  border: none;
+  padding: 20px;
+  cursor: pointer;
+  color: white;
+}
+
+.miet {
+  display: flex;
+  margin-left: 15px;
+  cursor: pointer;
+  background: #f9f9f9;
+  border-radius: 50%;
+  padding: 7px;
+}
+
+.miet:hover {
+  display: flex;
+  margin-left: 15px;
+  cursor: pointer;
+  background: #d9d9d9;
   border-radius: 50%;
   padding: 7px;
 }
