6c3d6088b12e57b3839cef65fbb23c3e74435cc7
siam <mdsiamsheikh6640@gmail.com>
2024-03-11 01:25:22 +0600

    creat new stylel hover add

--- components/header.vue
+++ components/header.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="header">
     <div class="header_left">
-      <svg
-        viewBox="0 0 24 24"
-        preserveAspectRatio="xMidYMid meet"
-        focusable="false"
-        class="style-scope yt-icon"
-        style="pointer-events: none; display: block; width: 30px; height: 30px"
-      >
-        <g class="style-scope yt-icon">
-          <path
-            d="M21,6H3V5h18V6z M21,11H3v1h18V11z M21,17H3v1h18V17z"
-            class="style-scope yt-icon"
-          ></path>
-        </g>
-      </svg>
-
+      <div class="header_left-icon">
+        <svg
+          viewBox="0 0 24 24"
+          preserveAspectRatio="xMidYMid meet"
+          focusable="false"
+          class="style-scope yt-icon"
+          style="
+            pointer-events: none;
+            display: block;
+            width: 30px;
+            height: 30px;
+          "
+        >
+          <g class="style-scope yt-icon">
+            <path
+              d="M21,6H3V5h18V6z M21,11H3v1h18V11z M21,17H3v1h18V17z"
+              class="style-scope yt-icon"
+            ></path>
+          </g>
+        </svg>
+      </div>
       <svg
         viewBox="0 0 90 20"
         preserveAspectRatio="xMidYMid meet"
@@ -75,46 +81,6 @@
         </g>
       </svg>
     </div>
-    <!-- <div class="header_search">
-      <div class="header-searc-button">
-        <div class="action">
-          <input type="text" placeholder="Seach" />
-        </div>
-        <button>
-          <svg
-            viewBox="0 0 24 24"
-            preserveAspectRatio="xMidYMid meet"
-            focusable="false"
-            class="style-scope yt-icon"
-            style="pointer-events: none; display: block; width: 25; height: 30"
-          >
-            <g class="style-scope yt-icon">
-              <path
-                d="M20.87,20.17l-5.59-5.59C16.35,13.35,17,11.75,17,10c0-3.87-3.13-7-7-7s-7,3.13-7,7s3.13,7,7,7c1.75,0,3.35-0.65,4.58-1.71 l5.59,5.59L20.87,20.17z M10,16c-3.31,0-6-2.69-6-6s2.69-6,6-6s6,2.69,6,6S13.31,16,10,16z"
-                class="style-scope yt-icon"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </div>
-      <div class="miet">
-        <svg
-          viewBox="0 0 24 24"
-          preserveAspectRatio="xMidYMid meet"
-          focusable="false"
-          class="style-scope yt-icon"
-          style="pointer-events: none; display: block; width: 27; height: 27"
-        >
-          <g class="style-scope yt-icon">
-            <path
-              d="M12 3C10.34 3 9 4.37 9 6.07V11.93C9 13.63 10.34 15 12 15C13.66 15 15 13.63 15 11.93V6.07C15 4.37 13.66 3 12 3ZM18.5 12H17.5C17.5 15.03 15.03 17.5 12 17.5C8.97 17.5 6.5 15.03 6.5 12H5.5C5.5 15.24 7.89 17.93 11 18.41V21H13V18.41C16.11 17.93 18.5 15.24 18.5 12Z"
-              class="style-scope yt-icon"
-            ></path>
-          </g>
-        </svg>
-      </div>
-    </div> -->
-
     <div class="header_search">
       <div class="header-search-button">
         <div class="action">
@@ -157,36 +123,40 @@
 
     <div class="header_icons">
       <div class="header_icon">
-        <svg
-          viewBox="0 0 24 24"
-          preserveAspectRatio="xMidYMid meet"
-          focusable="false"
-          class="style-scope yt-icon"
-          style="pointer-events: none; display: block; width: 25; height: 25"
-        >
-          <g class="style-scope yt-icon">
-            <path
-              d="M14,13h-3v3H9v-3H6v-2h3V8h2v3h3V13z M17,6H3v12h14v-6.39l4,1.83V8.56l-4,1.83V6 M18,5v3.83L22,7v8l-4-1.83V19H2V5H18L18,5 z"
-              class="style-scope yt-icon"
-            ></path>
-          </g>
-        </svg>
+        <div class="header_icon-svg">
+          <svg
+            viewBox="0 0 24 24"
+            preserveAspectRatio="xMidYMid meet"
+            focusable="false"
+            class="style-scope yt-icon"
+            style="pointer-events: none; display: block; width: 25; height: 25"
+          >
+            <g class="style-scope yt-icon">
+              <path
+                d="M14,13h-3v3H9v-3H6v-2h3V8h2v3h3V13z M17,6H3v12h14v-6.39l4,1.83V8.56l-4,1.83V6 M18,5v3.83L22,7v8l-4-1.83V19H2V5H18L18,5 z"
+                class="style-scope yt-icon"
+              ></path>
+            </g>
+          </svg>
+        </div>
       </div>
       <div class="header_icon">
-        <svg
-          viewBox="0 0 24 24"
-          preserveAspectRatio="xMidYMid meet"
-          focusable="false"
-          class="style-scope yt-icon"
-          style="pointer-events: none; display: block; width: 25; height: 25"
-        >
-          <g class="style-scope yt-icon">
-            <path
-              d="M10,20h4c0,1.1-0.9,2-2,2S10,21.1,10,20z M20,17.35V19H4v-1.65l2-1.88v-5.15c0-2.92,1.56-5.22,4-5.98V3.96 c0-1.42,1.49-2.5,2.99-1.76C13.64,2.52,14,3.23,14,3.96l0,0.39c2.44,0.75,4,3.06,4,5.98v5.15L20,17.35z M19,17.77l-2-1.88v-5.47 c0-2.47-1.19-4.36-3.13-5.1c-1.26-0.53-2.64-0.5-3.84,0.03C8.15,6.11,7,7.99,7,10.42v5.47l-2,1.88V18h14V17.77z"
-              class="style-scope yt-icon"
-            ></path>
-          </g>
-        </svg>
+        <div class="header_icon-svg">
+          <svg
+            viewBox="0 0 24 24"
+            preserveAspectRatio="xMidYMid meet"
+            focusable="false"
+            class="style-scope yt-icon"
+            style="pointer-events: none; display: block; width: 25; height: 25"
+          >
+            <g class="style-scope yt-icon">
+              <path
+                d="M10,20h4c0,1.1-0.9,2-2,2S10,21.1,10,20z M20,17.35V19H4v-1.65l2-1.88v-5.15c0-2.92,1.56-5.22,4-5.98V3.96 c0-1.42,1.49-2.5,2.99-1.76C13.64,2.52,14,3.23,14,3.96l0,0.39c2.44,0.75,4,3.06,4,5.98v5.15L20,17.35z M19,17.77l-2-1.88v-5.47 c0-2.47-1.19-4.36-3.13-5.1c-1.26-0.53-2.64-0.5-3.84,0.03C8.15,6.11,7,7.99,7,10.42v5.47l-2,1.88V18h14V17.77z"
+                class="style-scope yt-icon"
+              ></path>
+            </g>
+          </svg>
+        </div>
       </div>
       <div class="header_icon">
         <img
@@ -227,77 +197,28 @@ body {
 .header_left {
   display: flex;
   align-items: center;
-  margin-left: 15px;
-}
-
-.header_left svg {
-  margin-left: 15px;
-}
-
-.header svg {
-  padding: 0px 1px;
+  margin-left: 35px;
   cursor: pointer;
+  gap: 20px;
 }
 
-/* .header_search {
+.header_left-icon {
   display: flex;
   align-items: center;
-}
-
-.header_search .action {
-  border: 1px solid #cfcbcb;
-  height: 35px;
-  margin: 0;
-  padding: 0;
-  display: flex;
-  position: relative;
-}
-
-.header-searc-button {
-  display: flex;
-  height: 35px;
-}
-
-.action {
-  border: #f9f9f9;
-  border-radius: 5rem;
-}
-
-.header_search input {
-  width: 500px;
-  padding: 15px;
-  font-size: 16px;
-  outline: none;
-  border: none;
-}
-
-.header_search button {
-  padding: 0;
-  margin: 0;
-  height: 100%;
-  width: 50px;
-  background: #f9f9f9;
-  border: 1px solid #ddd9d9;
-}
-
-.header_search button:hover {
-  background: #f0f0f0;
-}
-
-.header_search button svg {
-  display: flex;
-  align-content: center;
-  margin-left: 0.7rem;
-}
-
-.miet {
-  display: flex;
-  margin-left: 15px;
+  width: 36px;
+  height: 36px;
+  padding: 4px;
   cursor: pointer;
-  background: #f9f9f9;
+}
+
+.header_left-icon:hover {
+  display: flex;
+  width: 36px;
+  height: 36px;
   border-radius: 50%;
-  padding: 7px;
-} */
+  padding: 4px;
+  background-color: #e5e5e5;
+}
 
 .header_search {
   display: flex;
@@ -367,6 +288,20 @@ button {
   display: flex;
   align-items: center;
   margin: 5px;
+}
+
+.header_icon-svg {
+  display: flex;
+  width: 40px;
+  padding: 7px;
+}
+
+.header_icon-svg:hover {
+  display: flex;
+  width: 40px;
+  background-color: #e5e5e5;
+  padding: 7px;
+  border-radius: 50%;
 }
 
 .header_icon img {
